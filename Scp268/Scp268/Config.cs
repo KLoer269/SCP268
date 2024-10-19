e932b272dcb0a5a02ab23bc3aa6b78a3a5ae8642
KLoer269 <leexxa123677@gmail.com>
2024-10-20 01:04:10 +0300

    Add files via upload

---
+++ Scp268/Scp268/Config.cs
@@ -0,0 +1,11 @@
+﻿using Exiled.API.Interfaces;
+
+
+namespace Scp268
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = true;
+    }
+}
